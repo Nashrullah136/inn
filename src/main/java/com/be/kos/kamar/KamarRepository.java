5f1e5268c0d87019a64395689f8ed7c20b8fc6de
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2022-11-09 20:54:52 +0700

    add function to find kamar by kosan id

--- src/main/java/com/be/kos/kamar/KamarRepository.java
+++ src/main/java/com/be/kos/kamar/KamarRepository.java
@@ -1,6 +1,10 @@
 package com.be.kos.kamar;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 public interface KamarRepository extends CrudRepository<Kamar, Long> {
+    List<Kamar> findByKosan_Id(@NonNull Long id);
 }
